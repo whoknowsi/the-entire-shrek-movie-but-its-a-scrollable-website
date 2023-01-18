5e096bbba3f2bb977b6fe132dccdabc0a9592f01
whoknowsi <echosmania@gmail.com>
2023-01-17 21:42:50 -0300

    Fixed things

--- pages/index.js
+++ pages/index.js
@@ -2,69 +2,76 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/index.module.css'
 import { useState, useEffect, useRef, use } from 'react'
-import { FaPlay, FaStop } from 'react-icons/fa'
+import { FaPlay, FaPause } from 'react-icons/fa'
 
 export default function Home() {
   const [images, setImages] = useState([])
   const [isPlaying, setIsPlaying] = useState(false)
-  const [isOnTop, setIsOnTop] = useState(true)
   const [frameInterval, setFrameInterval] = useState(null)
-  const counter = useRef(0)
+  const [hideTimeOut, setHideTimeOut] = useState(false)
+  const [counter, setCounter] = useState(1)
+  const counterRef = useRef(1)
 
   useEffect(() => {
-    const main = document.querySelector('main')
-
-    const importAll = (r) => {
-      return r.keys().map(r);
-    }
-
+    const importAll = (r) =>  r.keys().map(r)
     setImages(importAll(require.context('../public/shrek/', false, /\.(png|jpe?g|svg)$/)))
-    
-    const handleEvent = (e) => {
+  }, [])
 
+  // useEffect(() => {
+  //   const main = document.querySelector('main')
+  //   main.style.paddingTop = ((window.innerHeight - document.querySelector("img")?.offsetHeight) / 2) + 'px'
+  //   main.style.gap = ((window.innerHeight - document.querySelector("img")?.offsetHeight) / 2) + 'px'
+  //   main.style.paddingBottom = ((window.innerHeight - document.querySelector("img")?.offsetHeight) / 2) + 'px'
+  // }, [images])
+
+  useEffect(() => {
+    const pxToNum = (px) => Number(px.split('px')[0])
+
+    const handleEvent = (e) => {
       e.preventDefault()
       e.deltaY > 0 
-        ? counter.current += 1
-        : counter.current -= 1
-
-      counter.current === 1 && setIsOnTop(false)
-      counter.current === 0 && setIsOnTop(true)
-
-      window.scrollTo(0, counter.current * document.querySelector("img").offsetHeight)
+        ? setCounter(counter + 1)
+        : setCounter(counter - 1)
     }
 
-    let timeout
+    const main = document.querySelector('main')
+    const mainStyles = getComputedStyle(main)
+    const gap = mainStyles.getPropertyValue('gap')
 
-    const handleMouseMove = () => {
-      document.body.classList.remove('hide')
-      if(timeout) clearTimeout(timeout)
-      timeout = setTimeout(() => {
-        document.body.classList.add('hide')
-      }, 2000)
-    }
+    window.scrollTo(0, counter * (document.querySelector("img")?.offsetHeight + pxToNum(gap)))
+    
+    counterRef.current = counter  
 
     main.addEventListener('scroll', handleEvent, false)
     main.addEventListener('mousewheel', handleEvent, false)
     main.addEventListener('touchmove', handleEvent, false)
-    window.addEventListener('mousemove', handleMouseMove)
   
     return () => {
       main.removeEventListener('scroll', handleEvent, false)
       main.removeEventListener('mousewheel', handleEvent, false)
       main.removeEventListener('touchmove', handleEvent, false)
-      window.removeEventListener('mousemove', handleMouseMove)
     }
-  }, [])
+  }, [counter])
+
+  useEffect(() => {
+    const handleMouseMove = () => {
+      document.body.classList.remove('hide')
+      if(hideTimeOut) clearTimeout(hideTimeOut)
+      setHideTimeOut(setTimeout(() => {
+        document.body.classList.add('hide')
+      }, 2000))
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return () => window.removeEventListener('mousemove', handleMouseMove)
+  }, [hideTimeOut])
 
   const handlePlay = (e) => {
-    console.log("here")
     if(isPlaying) return
 
     setFrameInterval(setInterval(() => {
-      counter.current += 1
-      counter.current === 1 && setIsOnTop(false)
-
-      window.scrollTo(0, counter.current * document.querySelector("img").offsetHeight)
+      setCounter(counterRef.current + 1)
     }, 200))
 
     setIsPlaying(true)
@@ -81,8 +88,10 @@ export default function Home() {
     setIsPlaying(false)
   }
 
-  const toNum = (x) => {
-    return Number(x.default.src.split('out')[1].split('.')[0])
+  const toNum = (x) =>  Number(x.default.src.split('out')[1].split('.')[0])
+
+  const handleSlider = ({ target }) => {
+    setCounter(Number(target.value))
   }
 
   return (
@@ -97,15 +106,16 @@ export default function Home() {
         display: "flex",
         flexDirection: "column"
       }}>
-        <button className={`${styles.stopButton} ${!isPlaying ? styles.hidden : ''}`} onClick={handleStop}>
-          <FaStop/>
-        </button>
-        <button className={`${styles.playButton} ${!isOnTop ? styles.onBot : ''} ${isPlaying ? styles.hidden : ''}`} onClick={handlePlay}>
-          <FaPlay/>
-        </button>
+        <div className={styles.controlers}>
+          <button className={`${styles.stopButton} ${!isPlaying ? styles.hidden : ''}`} onClick={handleStop}>
+            <FaPause />
+          </button>
+          <button className={`${styles.playButton} ${isPlaying ? styles.hidden : ''}`} onClick={handlePlay}>
+            <FaPlay />
+          </button>
+          <input className={styles.slider} min="0" max={images.length} value={counter} onChange={handleSlider} type="range"></input>
+        </div>
         {images.sort((a, b) => toNum(a) - toNum(b)).map((image, i) => <Image style={{
-          width: "auto",
-          height: "100vh",
           pointerEvents: "none"
         }} key={i} src={image} width="1280" height="720" alt="shrek image"/>)}
       </main>
